485766eade3e1ba987e6fa18c6bbd76e2cfdc483
CM Student <cmstudent@uhwo-acm-10.local>
2018-04-02 11:02:13 -1000

    Completed Hackwich 8

--- Hackwich 8/ViewController.swift
+++ Hackwich 8/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    let initialLocation = CLLocation(latitude: 21.356505, longitude: -158.056071)
+    let regionRadius: CLLocationDistance = 10000
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+super.viewDidLoad()
+        centerMapLocation(location: initialLocation)
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
+        mapView.addAnnotation(restaurantOne)
+}
+    func centerMapLocation (location: CLLocation){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius,regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
